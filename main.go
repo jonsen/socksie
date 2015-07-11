fe751da882f5730e7ca12079aa443f6ac5322eaf
Jonsen Yang <im16hot@gmail.com>
2015-07-11 15:11:03 +0800

    add auto reconnect ssh server

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ package main
 // SSH to a remote host
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/golang/crypto/ssh"
 	"log"
 	"net"
 	"os"
+	"time"
 
 	"github.com/davecheney/profile"
 )
@@ -20,6 +22,9 @@ var (
 	PORT  = flag.Int("port", 7070, "socksie listening port")
 	RPORT = flag.Int("rport", 22, "ssh server port")
 	PASS  = flag.String("pass", os.Getenv("SOCKSIE_SSH_PASSWORD"), "ssh password")
+
+	// ssh client hand
+	conn *ssh.Client
 )
 
 func init() { flag.Parse() }
@@ -28,9 +33,7 @@ type Dialer interface {
 	DialTCP(net string, laddr, raddr *net.TCPAddr) (net.Conn, error)
 }
 
-func main() {
-	defer profile.Start(profile.CPUProfile).Stop()
-
+func sshDial() (err error) {
 	var auths []ssh.AuthMethod
 	if *PASS != "" {
 		auths = append(auths, ssh.Password(*PASS))
@@ -41,13 +44,60 @@ func main() {
 		Auth: auths,
 	}
 	addr := fmt.Sprintf("%s:%d", *HOST, *RPORT)
-	conn, err := ssh.Dial("tcp", addr, config)
+	conn, err = ssh.Dial("tcp", addr, config)
+
 	if err != nil {
-		log.Fatalf("unable to connect to [%s]: %v", addr, err)
+		errstr := fmt.Sprintf("unable to connect to [%s]: %v", addr, err)
+		return errors.New(errstr)
 	}
+
+	return
+}
+
+func sshCheck() (ok bool) {
+
+	session, err := conn.NewSession()
+	if err != nil {
+		log.Println("create session of ssh error", err)
+		return false
+	} else {
+		//log.Println("create session ok")
+	}
+
+	defer session.Close()
+
+	return true
+}
+
+func sshReconnect() {
+
+	for {
+
+		if !sshCheck() {
+			if err := sshDial(); err != nil {
+				log.Println(err)
+			} else {
+				log.Println("reconnect ssh ok")
+			}
+		}
+		time.Sleep(5 * time.Second)
+	}
+
+}
+
+func main() {
+	defer profile.Start(profile.CPUProfile).Stop()
+
+	err := sshDial()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	defer conn.Close()
 
-	addr = fmt.Sprintf("%s:%d", "0.0.0.0", *PORT)
+	go sshReconnect()
+
+	addr := fmt.Sprintf("%s:%d", "0.0.0.0", *PORT)
 	l, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("unable to listen on SOCKS port [%s]: %v", addr, err)
